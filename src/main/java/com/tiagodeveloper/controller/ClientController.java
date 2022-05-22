d12167186a499eb2064cd053788613023aa2c83a
Tiago Silva <programtiago@gmail.com>
2022-05-22 16:03:57 +0100

    Endpoint find added

--- src/main/java/com/tiagodeveloper/controller/ClientController.java
+++ src/main/java/com/tiagodeveloper/controller/ClientController.java
@@ -1,24 +1,33 @@
 package com.tiagodeveloper.controller;
 
 import com.tiagodeveloper.domain.Client;
+import com.tiagodeveloper.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClientRepository clientRepository;
 
-
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> getClients(){
-     return manager.createQuery("from Client", Client.class)
-             .getResultList();
+     return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> find(@PathVariable Long id){
+            return clientRepository.findById(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
     }
 
 
