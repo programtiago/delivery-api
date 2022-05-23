5e6cf55c45aec154810cf074dead8e0124b24a44
Tiago Silva <programtiago@gmail.com>
2022-05-23 19:01:02 +0100

    Endpoints add, update and delete

--- src/main/java/com/tiagodeveloper/controller/ClientController.java
+++ src/main/java/com/tiagodeveloper/controller/ClientController.java
@@ -3,11 +3,9 @@ package com.tiagodeveloper.controller;
 import com.tiagodeveloper.domain.Client;
 import com.tiagodeveloper.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,35 @@ public class ClientController {
             return clientRepository.findById(id)
                     .map(ResponseEntity::ok)
                     .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client add(@RequestBody Client client){
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/{clientId}")
+    public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client){
+        if (!clientRepository.existsById(clientId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        client.setId(clientId);
+        client = clientRepository.save(client);
+
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{clientId}")
+    public ResponseEntity<Void> delete(Long clientId){
+        if (!clientRepository.existsById(clientId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        clientRepository.deleteById(clientId);
+
+        return ResponseEntity.noContent().build();
     }
 
 
